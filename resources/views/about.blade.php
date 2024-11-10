cc2c28f96b73c3f372d8e4cd259802ae98622a52
IamMark19 <markanthonyudarbe19@gmail.com>
2024-11-10 19:47:01 +0800

    add relationship and pivot table relationship

--- resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:headings>
-        About Page
-    </x-slot:headings>
-    <h1>Hello this is About</h1>
-</x-layout>
---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    <x-slot:headings>
+       Job Listing
+    </x-slot:headings>
+    <ul>
+        @foreach ($jobs as $job)
+            <li>
+                <a href="/jobs/{{$job['id']}}">
+                    <strong>{{$job['title']}}</strong> Pays >{{$job['salary']}} per year.
+                </a>
+            </li>
+        @endforeach
+    </ul>
+
+</x-layout>
