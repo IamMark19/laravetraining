5e31d044aa3a5331ffe9a43daff5e579fc507e97
IamMark19 <markanthonyudarbe19@gmail.com>
2024-11-12 13:42:13 +0800

    resources in controller

--- routes/web.php
+++ routes/web.php
@@ -1,79 +1,19 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
-
-
-
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function ()  {
-
-    return view('jobs.index',[
-    'jobs' => Job::with('employer')->latest()->simplepaginate(3)
-    ]);
-});
-
-Route::get('/jobs/create', function ()  {
-
-    return view('jobs.create');
-});
-
-Route::get('/jobs/{id}', function ($id) {
-
-    $job = Job::find($id);
-
-    return view('jobs.show',['job' => $job]);
-});
-
-Route::post('/jobs', function ()  {
-    request()->validate([
-        'title' => 'required',
-        'salary' => 'required'
-    ]);
-
-   Job::create([
-    'title' => request('title'),
-    'salary' => request('salary'),
-    'employer_id' => 1
-   ]);
-
-   return redirect('/jobs');
-});
-
-Route::get('/jobs/{id}/edit', function ($id) {
-
-    $job = Job::find($id);
-
-    return view('jobs.edit',['job' => $job]);
-});
-
-Route::patch('/jobs/{id}', function ($id) {
-    request()->validate([
-        'title' => 'required',
-        'salary' => 'required'
-    ]);
-
-    $job = Job::findOrFail($id);
-
-    $job->update([
-    'title' => request('title'),
-    'salary' => request('salary'),
-   ]);
-
-    return redirect('/jobs/'.$job->id);
-});
-
-Route::delete('/jobs/{id}', function ($id) {
-
-    $job = Job::findOrFail($id);
-    $job->delete();
-
-    return redirect('/jobs');
-});
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create',  [JobController::class, 'create']);
+Route::get('/jobs/{job}',  [JobController::class, 'show']);
+Route::post('/jobs',  [JobController::class, 'store']);
+Route::get('/jobs/{job}/edit',  [JobController::class, 'edit']);
+Route::patch('/jobs/{job}',  [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 Route::get('/contact', function () {
     return view('contact');
